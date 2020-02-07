4761d143e3d460314cf646a12118477d6ce6ce19
CN22 <codenation@CN22s-MacBook-Pro.local>
2020-02-07 11:55:40 +0000

    final push

--- script.js
+++ script.js
@@ -1,57 +1,119 @@
-let score = document.getElementsByTagName("p")[1]
-let roll = document.getElementsByTagName("p")[2]
+let score1 = document.getElementsByTagName("p")[1]
+let score2 = document.getElementsByTagName("p")[2]
+let roll = document.getElementsByTagName("p")[3]
 let button = document.getElementById("roller")
 let reset = document.getElementById("reset")
 let dice = document.getElementById("dice")
-let current = 0
+let player1 = 0
+let player2 = 0
+let turn = 0
+
+
+    
 
 document.addEventListener("click",(event)=> {
     if (event.target == button) {
         let num = Math.ceil(Math.random()*6)
-        current+=num
-        score.textContent = `Your current score is ${current}`
+        if (turn % 2 == 0){
+            player1 += num
+        }
+
+        else{
+            player2 += num
+        }
+
+        score1.textContent = `Your current score is ${player1}`
+        score2.textContent = `Your current score is ${player2}`
         switch (true) {
-            case num == 1:
-                roll.textContent = `You rolled a one! You suck kiddo, get outta here!`
+
+            case 
+            
+                num == 1:
+                if (turn % 2 ==0){
+
+                    score1.textContent = "Player1 Looses and must now die"
+                    score2.textContent = "Player2 wins and is now free"
+                }
+                else {
+                    score1.textContent = "Player1 wins and is now free"
+                    score2.textContent ="Player2 looses and must now die"
+                }
                 button.style.display = "none"
                 reset.style.display = "inline"
                 dice.style.backgroundImage = "url(img/dice1.png)"
                 break
-            case num == 2:
+
+            case 
+            
+                num == 2:
+
                 roll.textContent = `You rolled  two!`
                 dice.style.backgroundImage = "url(img/dice2.png)"
                 break
-            case num == 3:
+
+            case 
+                num == 3:
+
                 roll.textContent = `You rolled  three!`
                 dice.style.backgroundImage = "url(img/dice3.png)"
                 break
-            case num == 4:
+
+            case 
+                num == 4:
+
                 roll.textContent = `You rolled  four!`
                 dice.style.backgroundImage = "url(img/dice4.png)"
                 break
-            case num == 5:
+
+            case 
+                num == 5:
+
                 roll.textContent = `You rolled  five!`
                 dice.style.backgroundImage = "url(img/dice5.png)"
                 break
-            case num == 6:
+
+            case 
+                num == 6:
+
                 roll.textContent = `You rolled  six!`
                 dice.style.backgroundImage = "url(img/dice6.png)"
                 break
         }
     }
-    if (current >= 21) {
-        roll.textContent = `You have won with a score of ${current}. Double or nothing?`
+
+    turn++
+    if (turn % 2 ==0){
+
+        button.textContent = "Player 1 please roll the die"
+}
+    else{
+        button.textContent = "Player 2 please roll the die"
+    }
+
+
+
+
+
+    if (player1 >= 21) {
+        
+        roll.textContent = `You have won with a score of ${player1}. player 2 shall now die!?`
         button.style.display = "none"
         reset.style.display = "inline"
     }
-})
 
+
+    if (player2 >= 21) {
+        
+        roll.textContent = `You have won with a score of ${player2}. player 1 shall now die!?`
+        button.style.display = "none"
+        reset.style.display = "inline"
+}
+
+})
 document.addEventListener("click",(event)=>{
+
     if (event.target == reset) {
+
        window.location.reload(true)
     }
 })
-
-
-
-
